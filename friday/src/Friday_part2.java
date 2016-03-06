f8ce6541d3b324cf5fbcb4971b2841dcfac811cf
ArturNizamutdinov <nizamutdin.art@yandex.ru>
2016-03-06 10:32:50 +0300

    refactored 'Friday_part2'

--- friday/src/Friday_part2.java
+++ friday/src/Friday_part2.java
@@ -5,19 +5,43 @@ import java.util.*;
  */
 public class Friday_part2 {
     public static void main(String[] args) {
+        int month = ReadMonth();
+        int quantity = 0;
+        int friday = 6;
+        ArrayList list = new ArrayList();
+        Calendar calendar;
+
+        for (int i = 1800; i < 2016; i++) {
+            calendar = new GregorianCalendar(i, month - 1, 13);
+            if (friday == calendar.get(Calendar.DAY_OF_WEEK)) {
+                list.add(calendar.get(Calendar.YEAR));
+                quantity++;
+            }
+        }
+
+        System.out.println("Count of years = " + quantity);
+        System.out.println("List of years with friday 13 in this month:");
+
+        for (int i = 0; i < quantity; i++) {
+            System.out.println(Integer.parseInt(list.get(i).toString()));
+        }
+
+    }
+
+    private static int ReadMonth() {
         boolean errorFlag;
         int month = 0;
         String line;
         Scanner scanner = new Scanner(System.in);
 
         do {
-            System.out.print("Type month: ");
+            System.out.print("Type number of month: ");
             line = scanner.nextLine();
 
             try {
                 month = Integer.valueOf(line);
 
-                if (!(month > 0 && month < 13)) {
+                if (!isBetween(month, 0, 13)) {
                     System.out.println("Month should be between [1..12]!!!");
                     errorFlag = true;
                 } else {
@@ -29,24 +53,16 @@ public class Friday_part2 {
             }
         } while (errorFlag);
 
-        int quantity = 0;
-        int friday = 6;
-        ArrayList list = new ArrayList();
-        for (int i = 1800; i < 2016; i++) {
-            Calendar calendar = new GregorianCalendar(i, month - 1, 13);
-            int number = calendar.get(Calendar.DAY_OF_WEEK);
-            if (friday == number) {
-                list.add(calendar.get(Calendar.YEAR));
-                quantity++;
-            }
-        }
-        System.out.println("List of years with friday 13 in this month:");
-        for (int i = 0; i < quantity; i++) {
-            Object value = list.get(i);
-            int a = Integer.parseInt(value.toString());
-            System.out.println(a);
-        }
-        System.out.println("Count of years = " + quantity);
+        return month;
+    }
 
+    private static boolean isBetween(int x, int a, int b) {
+        if (a > b) {
+            a ^= b;
+            b ^= a;
+            a ^= b;
+        }
+
+        return x > a && x < b;
     }
 }
